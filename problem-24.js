d5e96ab930fcb932e10b66d65ee4269edda85caf
MehediIslamRipon <mehediislamripon@gmail.com>
2022-06-05 01:39:02 +0600

    updated the problem details

--- problem-24.js
+++ problem-24.js
@@ -1,3 +1,9 @@
+// problem details
+/* You crashed seeing a girl in the shopping mall. You asked for the girl's contact number at one point in the conversation. But the girl was very clever. Without giving her the whole number, she gave you the first nine digits and told you to think out the other two numbers.
+Now you need to write a program that will find the last two digits of the contact number.
+* There is a secret number that will use as a mobile operator network. But pretend like you don't know the secret number. ex: 54
+* You have to use a loop instead of trying the phone number, again and again, to connect the call. */
+
 // the secret number
 const secretNum = 54;
 
