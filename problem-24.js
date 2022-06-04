f90b4c3d4442b237475b023af8e00d61800e7433
MehediIslamRipon <mehediislamripon@gmail.com>
2022-06-05 01:13:56 +0600

    problem-24.js solved

--- problem-24.js
+++ problem-24.js
@@ -1,14 +1,23 @@
-// const inCompleteNum = '017979892';
+// the secret number
 const secretNum = 54;
 
 function findTwoSecretNum(tryNum) {
-   if (tryNum === '') {
-      return 'Please enter a valid number to try!';
-   }
+   let result;
 
    for (let i = 0; i <= 99; i++) {
-      console.log('' + tryNum + i);
+      if (result) {
+         break;
+      }
+      let singleToDouble = `${i}`.length === 1 ? `0${i}` : i;
+      result =
+         ('' + tryNum + singleToDouble).slice(-2) * 1 === secretNum
+            ? `Congrats! Your expected number is: ${
+                 '0' + tryNum + singleToDouble
+              }`
+            : false;
    }
+
+   return result;
 }
 
-findTwoSecretNum('017979892');
+console.log(findTwoSecretNum(017979892));
