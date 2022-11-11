5f04ceaaac476f8e7708563c1e32dce1353d8de3
anasmak04 <anasdev8@gmail.com>
2022-11-11 17:29:59 +0100

    add another problem

---
+++ problem-39.js
@@ -0,0 +1,11 @@
+// create a function that take one argument  and return sum n numbers;
+
+
+function sum(...args){
+    return args.reduce((a,b) => {
+        return a + b;
+   },0)
+}
+
+let result = sum(2,1,5,6,8);
+console.log(result);
